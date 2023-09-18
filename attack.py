44d7557bae0d0caaba2fa498b74980515adb033f
wjfu <wjfu@hust.edu.cn>
2023-09-18 23:24:23 +0800

    reproduce the existing attack.

--- attack.py
+++ attack.py
@@ -1,17 +1,23 @@
 import os
 import numpy as np
 import torch
+from tqdm import tqdm
+from torch.utils.data import DataLoader
 import logging
 import random
 
-from attack.attack_model_PFAMI import AttackModel
-from pythae.models import AutoModel
-from diffusers import DiffusionPipeline
-from datasets import Image, Dataset
+# from attack.attack_model import AttackModel
+
 import yaml
-from data.prepare import data_prepare
 from attack.utils import get_logger
+import datasets
+from datasets import Image, Dataset
+from accelerate import Accelerator
+import trl
 from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, AutoConfig
+
+os.environ['HTTP_PROXY'] = 'http://115.156.158.36:7890'
+os.environ['HTTPS_PROXY'] = 'http://115.156.158.36:7890'
 
 # Load config file
 with open("configs/config.yaml", 'r') as f:
@@ -39,100 +45,131 @@ random.seed(seed)
 torch.backends.cudnn.benchmark = False
 torch.backends.cudnn.deterministic = True
 
-
 ## Load generation models.
-if cfg['dataset'] == "tinyin":
-    if cfg["target_model"] == "diffusion":
-        target_path = os.path.join(PATH, 'diffusion_models/ddpm-tinyin-64-30k')
-        target_model = DiffusionPipeline.from_pretrained(target_path).to(device)
+config = AutoConfig.from_pretrained(cfg["model_name"])
+config.use_cache = False
+bnb_config = None
+torch_dtype = torch.bfloat16 if torch.cuda.is_bf16_supported() else torch.float16
+target_model = AutoModelForCausalLM.from_pretrained(cfg["target_model"], quantization_config=bnb_config,
+                                                    torch_dtype=torch_dtype,
+                                                    config=config)
+reference_model = AutoModelForCausalLM.from_pretrained(cfg["model_name"], quantization_config=bnb_config,
+                                                       torch_dtype=torch_dtype,
+                                                       config=config)
+# shadow_path = os.path.join(PATH, 'diffusion_models/ddpm-celeba-64-50k-shadow/checkpoint-247500')
+# shadow_model = DiffusionPipeline.from_pretrained(shadow_path).to(device)
+logger.info("Successfully load models")
 
-        # shadow_path = os.path.join(PATH, 'diffusion_models/ddpm-celeba-64-50k-shadow/checkpoint-247500')
-        # shadow_model = DiffusionPipeline.from_pretrained(shadow_path).to(device)
-        shadow_model = None
+# Load tokenizer.
+tokenizer = AutoTokenizer.from_pretrained(cfg["model_name"], add_eos_token=cfg["add_eos_token"],
+                                          add_bos_token=cfg["add_bos_token"], use_fast=True)
+if cfg["pad_token_id"] is not None:
+    logger.info("Using pad token id %d", cfg["pad_token_id"])
+    tokenizer.pad_token_id = cfg["pad_token_id"]
 
-        # reference_path = os.path.join(PATH, 'diffusion_models/ddpm-celeba-64-50k-reference/checkpoint-247500')
-        # reference_model = DiffusionPipeline.from_pretrained(reference_path).to(device)
-        reference_model = None
-    elif cfg["target_model"] == "vae":
-        target_path = sorted(os.listdir(PATH + '/VAEs/target_models_on_' + cfg["dataset"] + "_50k"))[-1]
-        target_model = AutoModel.load_from_folder(
-            os.path.join(PATH + '/VAEs/target_models_on_' + cfg["dataset"] + "_50k", target_path, 'final_model'))
-        target_model = target_model.to(device)
-
-        reference_path = sorted(os.listdir(PATH + '/VAEs/reference_models_on_' + cfg["dataset"] + "_50k"))[-1]
-        reference_model = AutoModel.load_from_folder(
-            os.path.join(PATH + '/VAEs/reference_models_on_' + cfg["dataset"] + "_50k", reference_path, 'final_model'))
-        reference_model = reference_model.to(device)
-
-        shadow_path = sorted(os.listdir(PATH + '/VAEs/shadow_models_on_' + cfg["dataset"] + "_50k"))[-1]
-        shadow_model = AutoModel.load_from_folder(
-            os.path.join(PATH + '/VAEs/shadow_models_on_' + cfg["dataset"] + "_50k", shadow_path, 'final_model'))
-        shadow_model = shadow_model.to(device)
-elif cfg['dataset'] == "celeba":
-    if cfg["target_model"] == "diffusion":
-        target_path = os.path.join(PATH, 'diffusion_models/ddpm-celeba-64-50k/checkpoint-247500')
-        target_model = DiffusionPipeline.from_pretrained(target_path).to(device)
-
-        shadow_path = os.path.join(PATH, 'diffusion_models/ddpm-celeba-64-50k-shadow/checkpoint-247500')
-        shadow_model = DiffusionPipeline.from_pretrained(shadow_path).to(device)
-
-        reference_path = os.path.join(PATH, 'diffusion_models/ddpm-celeba-64-50k-reference/checkpoint-247500')
-        reference_model = DiffusionPipeline.from_pretrained(reference_path).to(device)
-    elif cfg["target_model"] == "vae":
-        target_path = sorted(os.listdir(PATH + '/VAEs/target_models_on_' + cfg["dataset"] + "_50k"))[-1]
-        target_model = AutoModel.load_from_folder(
-            os.path.join(PATH + '/VAEs/target_models_on_' + cfg["dataset"] + "_50k", target_path,
-                         'final_model'))
-        target_model = target_model.to(device)
-
-        reference_path = sorted(os.listdir(PATH + '/VAEs/reference_models_on_' + cfg["dataset"] + "_50k"))[-1]
-        reference_model = AutoModel.load_from_folder(
-            os.path.join(PATH + '/VAEs/reference_models_on_' + cfg["dataset"] + "_50k", reference_path,
-                         'final_model'))
-        reference_model = reference_model.to(device)
-
-        shadow_path = sorted(os.listdir(PATH + '/VAEs/shadow_models_on_' + cfg["dataset"] + "_50k"))[-1]
-        shadow_model = AutoModel.load_from_folder(
-            os.path.join(PATH + '/VAEs/shadow_models_on_' + cfg["dataset"] + "_50k", shadow_path,
-                         'final_model'))
-        shadow_model = shadow_model.to(device)
-
-logger.info("Successfully loaded models!")
+if tokenizer.pad_token_id is None:
+    logger.info("Pad token id is None, setting to eos token id...")
+    tokenizer.pad_token_id = tokenizer.eos_token_id
 
 # Load datasets
-all_dataset = data_prepare(cfg['dataset'], mode="datasets")
+raw_datasets = datasets.load_dataset(cfg["dataset_name"], cfg["dataset_config_name"])
+if "validation" in raw_datasets.keys():
+    train_dataset = raw_datasets["train"]
+    validation_dataset = raw_datasets["validation"]
+else:
+    logger.info(
+        f"No validation set in the raw dataset, split {cfg['validation_split_percentage']}% from the training set")
+    train_dataset = datasets.load_dataset(
+        cfg["dataset_name"],
+        cfg["dataset_config_name"],
+        split=f"train[:{cfg['validation_split_percentage']}%]"
+    )
+    validation_dataset = datasets.load_dataset(
+        cfg["dataset_name"],
+        cfg["dataset_config_name"],
+        split=f"train[{cfg['validation_split_percentage']}%:]",
+    )
+logger.info("Successfully load datasets!")
 
-if cfg['dataset'] == "tinyin":
-    datasets = {
-        "target": {
-            "train": Dataset.from_dict(all_dataset[random.sample(range(0, 30000), cfg["sample_number"])]),
-            "valid": Dataset.from_dict(all_dataset[random.sample(range(30000, 35000), cfg["sample_number"])])
-                },
-        "shadow": {
-            "train": Dataset.from_dict(all_dataset[random.sample(range(35000, 65000), cfg["sample_number"])]),
-            "valid": Dataset.from_dict(all_dataset[random.sample(range(65000, 70000), cfg["sample_number"])])
-        },
-        "reference": {
-            "train": Dataset.from_dict(all_dataset[random.sample(range(70000, 100000), cfg["sample_number"])]),
-            "valid": Dataset.from_dict(all_dataset[random.sample(range(100000, 105000), cfg["sample_number"])])
-        }
-    }
-elif cfg['dataset'] == "celeba":
-    datasets = {
-        "target": {
-            "train": Dataset.from_dict(all_dataset[random.sample(range(0, 50000), cfg["sample_number"])]),
-            "valid": Dataset.from_dict(all_dataset[random.sample(range(50000, 60000), cfg["sample_number"])])
-                },
-        "shadow": {
-            "train": Dataset.from_dict(all_dataset[random.sample(range(60000, 110000), cfg["sample_number"])]),
-            "valid": Dataset.from_dict(all_dataset[random.sample(range(110000, 120000), cfg["sample_number"])])
-        },
-        "reference": {
-            "train": Dataset.from_dict(all_dataset[random.sample(range(120000, 170000), cfg["sample_number"])]),
-            "valid": Dataset.from_dict(all_dataset[random.sample(range(170000, 180000), cfg["sample_number"])])
-        }
-    }
-attack_model = AttackModel(target_model, datasets, reference_model, shadow_model, cfg=cfg)
-# attack_model.attack_demo(cfg, target_model)
-# attack_model.attack_model_training(cfg=cfg)
-attack_model.conduct_attack(cfg=cfg)
+train_dataset = trl.trainer.ConstantLengthDataset(
+    tokenizer,
+    train_dataset,
+    dataset_text_field="text",
+    seq_length=1024,
+    eos_token_id=tokenizer.eos_token_id,
+    shuffle=False,
+)
+validation_dataset = trl.trainer.ConstantLengthDataset(
+    tokenizer,
+    validation_dataset,
+    dataset_text_field="text",
+    seq_length=1024,
+    eos_token_id=tokenizer.eos_token_id,
+    shuffle=False,
+)
+train_dataloader = DataLoader(train_dataset)
+eval_dataloader = DataLoader(validation_dataset)
+
+accelerator = Accelerator()
+target_model, reference_model, train_dataloader, eval_dataloader = accelerator.prepare(target_model, reference_model, train_dataloader, eval_dataloader)
+# for step, batch in enumerate(train_dataloader):
+#     outputs = target_model(**batch)
+#     loss = outputs.loss
+
+losses = []
+losses_ref = []
+for step, batch in tqdm(enumerate(eval_dataloader)):
+    with torch.no_grad():
+        outputs = target_model(**batch)
+        outputs_ref = reference_model(**batch)
+
+    loss = outputs.loss
+    losses.append(accelerator.gather(loss.reshape(-1, 1)))
+    loss_ref = outputs_ref.loss
+    losses_ref.append(accelerator.gather(loss_ref.reshape(-1, 1)))
+losses = torch.cat(losses, dim=0)
+losses_ref = torch.cat(losses_ref, dim=0)
+sorted_ratio = sorted([l-l_ref for l,l_ref in zip (losses,losses_ref)])
+threshold_ref = sorted_ratio[int(0.1*len(sorted_ratio))]
+
+
+losses = []
+losses_ref = []
+for step, batch in enumerate(train_dataloader):
+    with torch.no_grad():
+        outputs = target_model(**batch)
+
+    loss = outputs.loss
+
+    losses.append(accelerator.gather(loss.reshape(-1, 1)))
+    with torch.no_grad():
+        outputs_ref = reference_model(**batch)
+    loss_ref = outputs_ref.loss
+    losses_ref.append(accelerator.gather(loss_ref.reshape(-1, 1)))
+
+accelerator.wait_for_everyone()
+losses = torch.cat(losses, dim=0)
+
+losses_ref = torch.cat(losses_ref, dim=0)
+lr_rat = [l - l_r for l, l_r in zip(losses, losses_ref)]
+guess_cor_ref = sum([1 for sample in lr_rat if sample < threshold_ref])
+print("correct cnt  ref is: ", guess_cor_ref, "all is: ", len(losses), "ratio is: ", guess_cor_ref/len(losses))
+# datasets = {
+#     "target": {
+#         "train": Dataset.from_dict(all_dataset[random.sample(range(0, 30000), cfg["sample_number"])]),
+#         "valid": Dataset.from_dict(all_dataset[random.sample(range(30000, 35000), cfg["sample_number"])])
+#     },
+#     "shadow": {
+#         "train": Dataset.from_dict(all_dataset[random.sample(range(35000, 65000), cfg["sample_number"])]),
+#         "valid": Dataset.from_dict(all_dataset[random.sample(range(65000, 70000), cfg["sample_number"])])
+#     },
+#     "reference": {
+#         "train": Dataset.from_dict(all_dataset[random.sample(range(70000, 100000), cfg["sample_number"])]),
+#         "valid": Dataset.from_dict(all_dataset[random.sample(range(100000, 105000), cfg["sample_number"])])
+#     }
+# }
+#
+# attack_model = AttackModel(target_model, datasets, reference_model, shadow_model, cfg=cfg)
+# # attack_model.attack_demo(cfg, target_model)
+# # attack_model.attack_model_training(cfg=cfg)
+# attack_model.conduct_attack(cfg=cfg)
