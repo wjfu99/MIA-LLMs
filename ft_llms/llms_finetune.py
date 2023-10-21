456e81f3f0d530b24da5c5a9ae3638eee7a2d3ba
wjfu <wjfu@hust.edu.cn>
2023-10-21 13:18:34 +0800

    Fine-tuning reference model for gpt-j.

--- ft_llms/llms_finetune.py
+++ ft_llms/llms_finetune.py
@@ -183,7 +183,7 @@ if __name__ == "__main__":
         train_dataset, valid_dataset = dataset_prepare(args, tokenizer=tokenizer)
         train_dataset = Dataset.from_dict(train_dataset[args.train_sta_idx:args.train_end_idx])
         valid_dataset = Dataset.from_dict(valid_dataset[args.eval_sta_idx:args.eval_end_idx])
-        # train_dataset = load_from_disk("./cache/wikitext/refer_dataset")
+        train_dataset = load_from_disk("./cache/wikitext/refer_dataset_gptj")
 
     logger.info(f"Training with {Accelerator().num_processes} GPUs")
     training_args = TrainingArguments(
